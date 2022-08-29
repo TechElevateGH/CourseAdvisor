6eebd0c98ecbfef7aa92e56d9d7638d50e97a36f
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-08-29 11:45:13 -0400

    Project init

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from relations import db
+
+# Initialize Flask
+app = Flask(__name__)
+
+# DB config
+db_filename = "course_advisor.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=["GET"])
+def home():
+    return "Welcome to Course Advisor - TechElevate"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
