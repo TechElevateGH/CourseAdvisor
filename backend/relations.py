6bf11d7194e29e1b4f1c05e186ba071673a5318e
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-09-05 13:05:41 -0400

    Added get_course_posts endpoint Added error and success response utils

--- backend/relations.py
+++ backend/relations.py
@@ -5,3 +5,17 @@ db = SQLAlchemy()
 
 class Course(db.Model):
     pass
+
+
+
+
+
+
+
+class Post(db.Model):
+    pass
+
+
+    def serialize_for_course(self):
+        """ An object representation of a post"""
+        pass
