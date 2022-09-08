2e5f3f7de9befafa8920e2d0c767746948651698
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-09-08 14:56:33 -0400

    Advice table changed to Post. Fields changed

--- backend/relations.py
+++ backend/relations.py
@@ -35,59 +35,58 @@ class Department(db.Model):
             "courses": [course.serialize() for course in self.courses]
         })
 
+
 class Course(db.Model):
     __tablename__ = "courses"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     university = db.Column(db.Integer, db.ForeignKey('university.id'),
-        nullable=False)
+                           nullable=False)
     posts = db.relationship('Post', backref='course')
     num_posts = db.Column(db.Integer)
     overall_rating = db.Column(db.Integer)
 
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         self.name = kwargs.get("name")
         self.university = kwargs.get("university")
-    
+
     def serialize(self):
         return ({
             "id": self.id,
-            "name":self.name,
-            "university":self.university,
+            "name": self.name,
+            "university": self.university,
             "posts": [post.serialize() for post in self.posts],
-            "num_posts":self.num_posts,
-            "overall_rating":self.overall_rating
+            "num_posts": self.num_posts,
+            "overall_rating": self.overall_rating
         })
- 
+
 
 class Post(db.Model):
     __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     course = db.Column(db.Integer, db.ForeignKey('course.id'),
-        nullable=False)
+                       nullable=False)
     author = db.Column(db.String, nullable=False)
     professor = db.Column(db.String)
     semester = db.Column(db.String)
     year = db.Column(db.String)
-    comment = db.Column(db.String,nullable =False)
+    comment = db.Column(db.String, nullable=False)
     rating = db.Column(db.Integer)
     time = db.Column(db.DateTime)
 
-    
     def __init__(self, **kwargs):
         self.comment = kwargs.get("comment")
         self.author = kwargs.get("author")
-    
+
     def serialize(self):
         return {
             "id": self.id,
-            "course":self.course,
-            "author":self.author,
-            "professor":self.professor,
-            "semester":self.semester,
-            "year":self.year,
-            "comment":self.comment,
-            "rating":self.rating,
+            "course": self.course,
+            "author": self.author,
+            "professor": self.professor,
+            "semester": self.semester,
+            "year": self.year,
+            "comment": self.comment,
+            "rating": self.rating,
             "time": self.time
         }
-
